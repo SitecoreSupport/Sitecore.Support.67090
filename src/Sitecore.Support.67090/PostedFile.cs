f280641ca8e150a5ac7b30694c7d348c7ea48146
Ivan Sheyenko <ivs@sitecore.net>
2017-04-06 16:30:40 +0300

    patch created

---
+++ src/Sitecore.Support.67090/PostedFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Form.Core.Media
+{
+    public class PostedFile: Sitecore.Form.Core.Media.PostedFile    
+    {
+        public PostedFile()
+        {
+        }
+
+        public PostedFile(byte[] data, string fileName, string destination) : base(data,fileName,destination)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return  this.Destination +"/"+this.FileName;
+        }
+    }
+}
---
+++ src/Sitecore.Support.67090/UploadFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Form.Core.Controls.Data;
+using Sitecore.Support.Form.Core.Media;
+
+namespace Sitecore.Support.Form.Web.UI.Controls
+{
+    public class UploadFile: Sitecore.Form.Web.UI.Controls.UploadFile
+    {
+        public override ControlResult Result
+        {
+            get
+            {
+                if (this.upload.HasFile)
+                {
+                    return new ControlResult(this.ControlName, new PostedFile(this.upload.FileBytes, this.upload.FileName, this.UploadTo), "medialink");
+                }
+                return new ControlResult(this.ControlName, null, string.Empty);
+            }
+
+            set
+            {
+               
+            }
+        }
+    }
+}
